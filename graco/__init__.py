384f65699ad580b0a8f818926d46bdeb37b6baa5
ClusterDuck123 <mk.youssef@hotmail.com>
2019-10-18 20:27:04 +0200

    First commit

---
+++ graco/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from graco.functions import Calculate
+
+orbits = Calculate.orbits
+clusterings = Calculate.coefficients
