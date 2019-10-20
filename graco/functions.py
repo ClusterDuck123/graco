b1796a46e1fce0875f17f4e48fada9c21d5e8fe1
ClusterDuck123 <mk.youssef@hotmail.com>
2019-10-20 23:56:29 +0200

    GEA is ready now.

--- graco/functions.py
+++ graco/functions.py
@@ -10,6 +10,9 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 import subprocess
+import os
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
 
 def tvd(P,Q):
    if   np.isnan(P).all() and np.isnan(Q).all():
@@ -18,32 +21,29 @@ def tvd(P,Q):
        return 1.
    else:
        return np.sum(np.abs(P-Q))/2
-   
-   
-def get_nx2orca_path():
-    return '/home/clusterduck/Parser/nx2orca.py'
-def get_orca_path():
-    return '/home/clusterduck/Programs/orca/orca.exe'
-def get_Pau_script():
-    return f'/home/clusterduck/Programs/Pau/script2.sh'
 
+
+def get_orca_path():
+    return f"{CURRENT_DIRECTORY}/orca/orca"
 def get_edgelist_path():
-    return f'/tmp/edgelist.tmp'
+    return f"{CURRENT_DIRECTORY}/tmp/edgelist.tmp"
 def get_orbits_path():
-    return f'/tmp/orbits.tmp'
+    return f"{CURRENT_DIRECTORY}/tmp/orbits.tmp"
 def get_coefficients_path():
-    return f'/tmp/clusterings.tmp'
+    return f"{CURRENT_DIRECTORY}/tmp/clusterings.tmp"
+
 
 def run_cmd(cmd):
-    completed_process = subprocess.run(cmd, 
-                                       stderr=subprocess.PIPE, 
+    completed_process = subprocess.run(cmd,
+                                       stderr=subprocess.PIPE,
                                        check=True)
     if completed_process.stderr:
         raise subprocess.CalledProcessError(cmd = cmd,
                     returncode = completed_process.returncode)
-        
-gcc_header = {'c_02' :r'$c_{0\rightarrow2}$' , 'c_03' :r'$c_{0\rightarrow3}$' , 
-               'c_15' :r'$c_{1\rightarrow5}$' , 'c_18' :r'$c_{1\rightarrow8}$' , 
+
+
+gcc_header = {'c_02' :r'$c_{0\rightarrow2}$' , 'c_03' :r'$c_{0\rightarrow3}$' ,
+               'c_15' :r'$c_{1\rightarrow5}$' , 'c_18' :r'$c_{1\rightarrow8}$' ,
                'c_110':r'$c_{1\rightarrow10}$', 'c_112':r'$c_{1\rightarrow12}$',
                'c_27' :r'$c_{2\rightarrow7}$' , 'c_211':r'$c_{2\rightarrow11}$',
                'c_213':r'$c_{2\rightarrow13}$',
@@ -54,27 +54,29 @@ class Write:
     def edgelist(G):
         file_in  = get_edgelist_path()
         nx.write_edgelist(G, file_in, data=False)
-        parser = get_nx2orca_path()
-        parser_cmd = ['python3', parser, file_in]
-        run_cmd(parser_cmd)
-    
+        N, M = G.number_of_nodes(), G.number_of_edges()
+        with open(file_in, 'r+') as f:
+            content = f.read()
+            f.seek(0)
+            f.write(f"{N} {M}\n" + content)
+
     @staticmethod
     def orbits(G, file_out=None, graphlet_nodes=4):
         Write.edgelist(G)
-        
+
         file_in  = get_edgelist_path()
-        
+
         if file_out is None:
             file_out = get_orbits_path()
-        
+
         orca     = get_orca_path()
         orca_cmd = [orca, str(graphlet_nodes), file_in, file_out]
         run_cmd(orca_cmd)
-        
+
     @staticmethod
     def coefficients(G):
         Write.orbits(G)
-        
+
         file_in  = get_orbits_path()
         file_out = get_coefficients_path()
 
@@ -86,15 +88,15 @@ class Write:
 class Calculate:
     @staticmethod
     def orbits(G, dtype=pd.DataFrame):
-        
+
         label_mapping   = {name:n for n,name in enumerate(G)}
         reverse_mapping = {value:key for key,value in label_mapping.items()}
-        
+
         G = nx.relabel_nodes(G, label_mapping)
-        
+
         Write.orbits(G=G)
         file_in = get_orbits_path()
-        
+
         if   dtype == pd.DataFrame:
             columnn_names = [f'o{i}' for i in range(15)]
             df = pd.read_csv(file_in, delimiter=' ', names=columnn_names)
@@ -104,24 +106,24 @@ class Calculate:
             return np.genfromtxt(file_in)
         else:
             raise TypeError('Please provide an appropriate type.')
-        
+
     @staticmethod
     def coefficients(G, dtype=pd.DataFrame):
-        
+
         label_mapping   = {name:n for n,name in enumerate(G)}
         reverse_mapping = {value:key for key,value in label_mapping.items()}
-        
+
         G = nx.relabel_nodes(G, label_mapping)
-        
+
         Write.coefficients(G=G)
         file_in = get_coefficients_path()
-        
+
         if   dtype == pd.DataFrame:
             columnn_names = ['c_02' , 'c_03',
                              'c_15' , 'c_18' , 'c_110', 'c_112',
                              'c_27' , 'c_211', 'c_213',
                              'c_311', 'c_313', 'c_314']
-        
+
             df = pd.read_csv(file_in, delimiter=' ', names=columnn_names)
             return df.rename(index=reverse_mapping)
 
@@ -129,5 +131,3 @@ class Calculate:
             return np.genfromtxt(file_in)
         else:
             raise TypeError('Please provide an appropriate type.')
-    
-    
